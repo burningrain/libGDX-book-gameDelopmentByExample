976076b26b55836197ce9e7d14005c34302dc02e
ash <lixkram@gmail.com>
2023-04-01 14:44:14 +0300

    - обновлен gradle до 8.0.2 - обновлены зависимости libgdx - обновлена java до 1.7 (требуют зависимости libgdx) в библиотеках, а на десктопе до 1.8 !!! необходимо добавить генерацию джава-классов и файликов filelist.txt в build.gradle модуля core для assets

--- chapter4/desktop/src/com/packt/flappeebee/desktop/DesktopLauncher.java
+++ chapter4/desktop/src/com/packt/flappeebee/desktop/DesktopLauncher.java
@@ -1,18 +1,21 @@
 package com.packt.flappeebee.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.packt.flappeebee.FlappeeBeeGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "crab";
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("crab");
 
-		config.width = 1024;
-		config.height = 768;
+		//config.width = 1024;
+		//config.height = 768;
 		//config.fullscreen = true;
-		config.samples = 3;
-		new LwjglApplication(new FlappeeBeeGame(), config);
+		//config.samples = 3;
+		//config.setFullscreenMode(Graphics.DisplayMode.);
+		new Lwjgl3Application(new FlappeeBeeGame(), config);
 	}
 }
--- chapter4/libgdx-simple-animation-generator/src/main/java/com/github/br/gdx/simple/animation/generator/JavaSourceFromString.java
+++ chapter4/libgdx-simple-animation-generator/src/main/java/com/github/br/gdx/simple/animation/generator/JavaSourceFromString.java
@@ -4,9 +4,10 @@ import javax.tools.SimpleJavaFileObject;
 import java.net.URI;
 
 public class JavaSourceFromString extends SimpleJavaFileObject {
-    final String code;
 
-    JavaSourceFromString(String name, String code) {
+    private final String code;
+
+    public JavaSourceFromString(String name, String code) {
         super(URI.create("string:///" + name.replace('.','/') + Kind.SOURCE.extension),Kind.SOURCE);
         this.code = code;
     }
