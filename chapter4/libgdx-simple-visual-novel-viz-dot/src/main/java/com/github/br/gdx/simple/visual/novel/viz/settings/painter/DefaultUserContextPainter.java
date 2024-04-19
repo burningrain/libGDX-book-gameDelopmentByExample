aa93c1fe1677d5937b32c6af5b6a5e48f78c14fe
ash <lixkram@gmail.com>
2024-04-19 19:34:40 +0300

    - немного доработал отображение контекста процесса и его содержимого
     !) выделить другим цветом текущую ноду
     !) научиться выводить diff между конеткстом в заранее помеченных точках процесса
    
     ! добавить возможность раскраски параметрам контекста
     ! добавить режимы SHORT/FULL развертки контекста, чтобы контекст мог выводиться полностью, а не только 1-го уровня
     ! выровнять легенду, контекст и процесс друг относительно друга хоть какими-либо правдами и неправдами.
    
     ! добавить расцветку параметров объектов
     x выводить пользовательские цвета и пояснения, что они означают (сделано)
     x выровнять легенду и основной граф друг относительно друга (невозможно выравнивать кластера в graphviz)
     ! добавить отображение данных по условиям перехода
     ! добавить описание нод во всплывающем окне в svg
     ! нету валидации графа. Вполне можно собрать такой процесс с вложенными процессами, который будет циклиться сам на себя бесконечно. Например: А->B->A->B->A... (A содержит B, который содержит A, который содержит...) и это не проверяется.

--- chapter4/libgdx-simple-visual-novel-viz-dot/src/main/java/com/github/br/gdx/simple/visual/novel/viz/settings/painter/DefaultUserContextPainter.java
+++ chapter4/libgdx-simple-visual-novel-viz-dot/src/main/java/com/github/br/gdx/simple/visual/novel/viz/settings/painter/DefaultUserContextPainter.java
@@ -5,8 +5,12 @@ import com.github.br.gdx.simple.visual.novel.viz.settings.DotVizSettings;
 import com.github.br.gdx.simple.visual.novel.viz.settings.color.DotColorsSchema;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.Map;
 
 public class DefaultUserContextPainter implements UserContextPainter {
+
+    public static final String INDENT = "    ";
 
     @Override
     public String createContext(DotVizSettings settings, UserContext userContext) {
@@ -23,31 +27,31 @@ public class DefaultUserContextPainter implements UserContextPainter {
         result.append("userContext [")
                 .append("shape=plaintext; ").append("label=").append("<").append("\n");
 
-        result.append("<table>").append("\n");
-        result.append("<tr>").append("\n")
-                .append("<td colspan=\"3\" bgcolor=\"").append(userContextHeaderColor).append("\">").append("\n")
+        result.append("<TABLE>").append("\n");
+        result.append("<TR>").append("\n")
+                .append("<TD COLSPAN=\"3\" BGCOLOR=\"").append(userContextHeaderColor).append("\">").append("\n")
                 .append(userContext.getClass().getSimpleName())
-                .append("</td>").append("\n")
-                .append("</tr>").append("\n")
+                .append("</TD>").append("\n")
+                .append("</TR>").append("\n")
         ;
-        result.append("<tr>").append("\n");
+        result.append("<TR>").append("\n");
         result
-                .append("<td bgcolor=\"").append(userContextHeaderColor).append("\">").append("name").append("</td>")
-                .append("<td bgcolor=\"").append(userContextHeaderColor).append("\">").append("type").append("</td>")
-                .append("<td bgcolor=\"").append(userContextHeaderColor).append("\">").append("value").append("</td>")
+                .append("<TD BGCOLOR=\"").append(userContextHeaderColor).append("\">").append("name").append("</TD>")
+                .append("<TD BGCOLOR=\"").append(userContextHeaderColor).append("\">").append("type").append("</TD>")
+                .append("<TD BGCOLOR=\"").append(userContextHeaderColor).append("\">").append("value").append("</TD>")
 
-                .append("</tr>").append("\n")
+                .append("</TR>").append("\n")
         ;
         try {
             fillUserParams(result, "", userContext);
         } catch (Exception e) {
-            result.append("<tr>").append("\n");
-            result.append("<td colspan=\"3\" bgcolor=\"").append(colorSchema.getErrorNodeColor()).append("\">").append("\n")
-                    .append(userContext.getClass().getSimpleName())
-                    .append("</td>").append("\n");
-            result.append("</tr>").append("\n");
+            result.append("<TR>").append("\n");
+            result.append("<TD COLSPAN=\"3\" BGCOLOR=\"").append(colorSchema.getErrorNodeColor()).append("\">").append("\n")
+                    .append(e.getMessage())
+                    .append("</TD>").append("\n");
+            result.append("</TR>").append("\n");
         }
-        result.append("</table>").append("\n");
+        result.append("</TABLE>").append("\n");
 
         result.append(">").append("]").append("\n");
         result.append("}");
@@ -58,19 +62,74 @@ public class DefaultUserContextPainter implements UserContextPainter {
         Class<?> clazz = object.getClass();
         Field[] declaredFields = clazz.getDeclaredFields();
         for (Field field : declaredFields) {
-            fillParam(result, indent, field, field.get(object));
+            String name = field.getName();
+
+            Type genericType = field.getGenericType();
+            String classNameString = genericType.toString();
+
+            field.setAccessible(true);
+            Object v = field.get(object);
+            if (v == null) {
+                v = "";
+            }
+            fillParam(result, indent, name, classNameString, v);
         }
     }
 
-    private void fillParam(StringBuilder result, String indent, Field field, Object value) throws IllegalAccessException {
-        Object v = value == null ? "" : field.get(value);
+    private void fillParam(StringBuilder result, String indent, String fieldName, String fieldClass, Object obj) {
+        String value = "";
+        if (!(obj instanceof Iterable || obj instanceof Map)) {
+            value = obj.toString();
+        }
+
         result
-                .append("<tr>").append("\n")
-                .append("<td>").append(indent).append(field.getName()).append("</td>").append("\n")
-                .append("<td>").append(indent).append(field.getClass().getName()).append("</td>").append("\n")
-                .append("<td>").append(indent).append(v).append("</td>").append("\n")
-                .append("</tr>").append("\n")
+                .append("<TR>").append("\n")
+                .append("<TD ALIGN=\"LEFT\">").append("<![CDATA[").append(indent).append(fieldName).append("]]>").append("</TD>").append("\n")
+                .append("<TD>").append("<![CDATA[").append(fieldClass).append("]]>").append("</TD>").append("\n")
+                .append("<TD>").append("<![CDATA[").append(value).append("]]>").append("</TD>").append("\n")
+                .append("</TR>").append("\n")
         ;
+
+        Class<?> clazz = obj.getClass();
+        // если тип простой, то дальше ничего не делаем
+        if (byte.class.isAssignableFrom(clazz) ||
+                short.class.isAssignableFrom(clazz) ||
+                int.class.isAssignableFrom(clazz) ||
+                long.class.isAssignableFrom(clazz) ||
+                float.class.isAssignableFrom(clazz) ||
+                double.class.isAssignableFrom(clazz) ||
+                boolean.class.isAssignableFrom(clazz) ||
+                Byte.class.isAssignableFrom(clazz) ||
+                Short.class.isAssignableFrom(clazz) ||
+                Integer.class.isAssignableFrom(clazz) ||
+                Long.class.isAssignableFrom(clazz) ||
+                Float.class.isAssignableFrom(clazz) ||
+                Double.class.isAssignableFrom(clazz) ||
+                Number.class.isAssignableFrom(clazz) ||
+                Boolean.class.isAssignableFrom(clazz) ||
+                String.class.isAssignableFrom(clazz)
+        ) {
+            return;
+        }
+        if (Object[].class.isAssignableFrom(clazz)) {
+            Object[] array = (Object[]) obj;
+            for (int i = 0; i < array.length; i++) {
+                fillParam(result, indent + INDENT, "[" + i + "]", array[i].getClass().getName(), array[i]);
+            }
+        } else if (Iterable.class.isAssignableFrom(clazz)) {
+            Iterable<?> list = (Iterable<?>) obj;
+            int i = 0;
+            for (Object o : list) {
+                fillParam(result, indent + INDENT, "[" + i + "]", o.getClass().getName(), o);
+                i++;
+            }
+        } else if (Map.class.isAssignableFrom(clazz)) {
+            for (Map.Entry<?, ?> entry : ((Map<?, ?>) obj).entrySet()) {
+                fillParam(result, indent + INDENT, entry.getKey().toString(), entry.getClass().getName(), entry.getValue());
+            }
+        }
     }
+
+    //TODO 1) подсветить текущую ноду 2) научиться выводить diff между конеткстом в заранее помеченных точках процесса
 
 }
--- chapter4/libgdx-simple-visual-novel-viz-dot/src/test/java/com/github/br/gdx/simple/visual/novel/viz/impl/TestUserContext.java
+++ chapter4/libgdx-simple-visual-novel-viz-dot/src/test/java/com/github/br/gdx/simple/visual/novel/viz/impl/TestUserContext.java
@@ -2,10 +2,30 @@ package com.github.br.gdx.simple.visual.novel.viz.impl;
 
 import com.github.br.gdx.simple.visual.novel.api.ElementId;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TestUserContext implements TestInterfaceUserContext {
 
     public ElementId nextId;
+    private final HashMap<String, Object> map = new HashMap<String, Object>() {{
+        put("obj", new Object());
+        put("21321", new A("ddd", 5));
+        put("2424", new A("3cassaadasd", 12345));
+        put("list", new ArrayList<Double>(){{
+            add(4d);
+            add(2323.223);
+            add(22_654_032.9292021);
+            add(32342.234234);
+        }});
+    }};
 
+    private final HashSet<String> set = new HashSet<String>() {{
+        add("the first");
+        add("the second");
+    }};
 
     @Override
     public void setNextId(ElementId elementId) {
@@ -15,6 +35,29 @@ public class TestUserContext implements TestInterfaceUserContext {
     @Override
     public ElementId getNextId() {
         return nextId;
+    }
+
+    public HashMap<String, Object> getMap() {
+        return map;
+    }
+
+    private static final class A {
+        private String str;
+        private int i;
+        private ReentrantLock reentrantLock = new ReentrantLock();
+
+        public A(String str, int i) {
+            this.str = str;
+            this.i = i;
+        }
+
+        public String getStr() {
+            return str;
+        }
+
+        public int getI() {
+            return i;
+        }
     }
 
 }
